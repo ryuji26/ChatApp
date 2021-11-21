e5586e291afb6edd8529ce65717f1c86605fe284
ryuji <ryujispjp@gmail.com>
2021-11-22 01:19:33 +0900

    first commit

---
+++ ChatApp/ChatListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatListViewController.swift
+//  ChatApp
+//
+//  Created by 髙橋　竜治 on 2021/11/21.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    @IBOutlet weak var chatListTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+class ChatListTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
