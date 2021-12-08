618d04c072d6cb7212fab9c78fdb63133ce9bc27
ryuji <ryujispjp@gmail.com>
2021-12-08 17:40:30 +0900

    table view 実装

--- ChatApp/ChatListViewController.swift
+++ ChatApp/ChatListViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ChatListViewController: UIViewController {
 
+    private let cellId = "chatListCell"
+
     @IBOutlet weak var chatListTableView: UITableView!
 
     override func viewDidLoad() {
@@ -18,10 +20,38 @@ class ChatListViewController: UIViewController {
 
 }
 
+extension ChatListViewController: UITableViewDelegate {
+
+}
+
+extension ChatListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatListTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        return cell
+    }
+
+
+}
+
 class ChatListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var partnerLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var latestMessageLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        userImageView.layer.cornerRadius = 35
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
